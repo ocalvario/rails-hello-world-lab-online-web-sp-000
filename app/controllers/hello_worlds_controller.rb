f52136b1e66a7a5c4f4942cd4444050a9ba03b6a
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-18 03:06:58 +0000

    Automatically backed up by Learn

--- app/controllers/hello_worlds_controller.rb
+++
@@ -1,5 +0,0 @@
-class HelloWorldsController < ApplicationController
-  def hello_world
-    render :hello_world
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get '/hello_world' => 'hello_worlds#hello_world'
+   root :to => "hw#hello_world"
 end
